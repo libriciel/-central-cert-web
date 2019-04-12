a6c85fe8f51c9e67810d11568372cb8a51e57111
Tilo <tilopapin@gmail.com>
2019-04-12 16:10:05 +0200

    clean

---
+++ frontend/src/app/app-routing.module.ts
@@ -0,0 +1,12 @@
+import { NgModule } from '@angular/core';
+import { Routes, RouterModule } from '@angular/router';
+import { CertificatListComponent } from './certificat-list/certificat-list.component';
+const routes: Routes = [
+  {path: 'certificats', component: CertificatListComponent}
+];
+
+@NgModule({
+  imports: [RouterModule.forRoot(routes)],
+  exports: [RouterModule]
+})
+export class AppRoutingModule { }
